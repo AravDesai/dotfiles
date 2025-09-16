c6dedd2588e3cdcd8426adecd990d9c9b5b807e7
AravDesai <aravdesai08@gmail.com>
2025-09-16 16:23:06 -0400

    Nvim: Made color viewing easier

--- nvim/lua/arav/plugins/telescope.lua
+++ nvim/lua/arav/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    
+
     telescope.setup({
       defaults = {
         path_display = { "smart" },
